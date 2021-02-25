b9554031c34d867f2cb8badf93daae9a190d3101
hackerman <mairo.aru@gmail.com>
2021-02-25 17:03:55 +0200

    fast car

---
+++ dude where is my car/Car.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dude_where_is_my_car
+{
+    class Car
+    {
+        private string _mark;
+        private string _model;
+        private string _regNr;
+        private string _color;
+        private int _odo;
+        private int _fuel = 60;
+        protected int _laps;
+        public Car(string mark,string model,string regNr,string color)
+        {
+            _color = color;
+            _laps = 0;
+            
+            _mark = mark;
+            _model = model;
+            
+            _regNr = regNr;
+            if(regNr.Length > 6)
+            {
+                
+                throw new ArgumentOutOfRangeException($"Reg nr cannot be longer than 6 characters. Current: {regNr}");
+            }
+            
+        }
+        public void Showcar()
+        {
+            Console.WriteLine($" mark:{_mark}\n color:{_color}\n model:{_model}\n regNr:{_regNr}\n Odo:{_odo}\n Fuel:{_fuel}\n Laps{_laps} ");
+        }
+        public void CarDrives()
+        {
+            Console.WriteLine($"Car drives 1 lap");
+            _laps += 1;
+            _odo += 100;
+            _fuel -= 5;
+        }
+        public void TankEmpty()
+        {
+            do
+            {
+                CarDrives();
+            } while (_fuel > 0);
+            Console.WriteLine($"car drove {_laps} laps");
+
+        }
+        public string Mark
+        {
+            get
+            {
+                return _mark;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+        }
+        public string REGnr
+        {
+            get
+            {
+                return _regNr;
+            }
+        }
+        public int Odometer
+        {
+            get
+            {
+                return _odo;
+            }
+        }
+        public int Fueltank
+        {
+            get
+            {
+                return _fuel;
+            }
+        }
+    }
+}
---
+++ dude where is my car/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dude_where_is_my_car
+{
+    class Program
+    {
+        /*
+         * Create a class Car
+The car has a mark name (string), model name (string), registration number (string, no longer than six characters), color (string), odometer (int) and fuel tank (int)
+The odometer value equals 0 by default.
+The fuel tank value by default is 60.
+No car characteristics can be changed after the car object has been created.*
+All the car characteristics can be accessed using ‘.’ (E.g. newCar.Model);
+The car can drive. Every lap the car passes increases the value of the odometer by 100 and decreases the value of the fuel tank by 5 .
+*/
+        static void Main(string[] args)
+        {
+            Car car = new Car("vw", "polo", "konnff", "blue");
+            car.Showcar();
+            
+            car.TankEmpty();
+            car.Showcar();
+
+        }
+    }
+}
